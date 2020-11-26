0ea1dd3403d7424a2ea3c401e586619fbd90ad6b
Deflaus <snow41521@gmail.com>
2020-11-26 11:17:55 +0300

    adding model of comics

--- Marvel/settings.py
+++ Marvel/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'comics',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,10 @@ WSGI_APPLICATION = 'Marvel.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'marvelapi',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
     }
 }
 
--- comics/models.py
+++ comics/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+
+class Comics(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    datetime_created = models.DateTimeField(auto_now_add=True, db_index=True)
+    thumbnail = models.ImageField(upload_to='thumbnails/')
+    images = ArrayField(models.ImageField(upload_to='images/'))
+    characters = ArrayField(models.CharField(max_length=200))
+    stories = ArrayField(models.CharField(max_length=200))
+
+    class Meta:
+        ordering = ('title',)
+
+    def __str__(self):
+        return f'Комикс {self.title}'
